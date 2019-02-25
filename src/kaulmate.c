c078712db1b2030d8dce4a7e05cf583acb49c144
domsson <julien.d.dau@gmail.com>
2019-02-26 06:13:09 +0900

    Changed code to use libtwirc instead of libircclient, some things changed to hardcoded for now though

--- src/kaulmate.c
+++ src/kaulmate.c
@@ -11,6 +11,7 @@
 #include "ini.h"
 #include "libircclient.h"
 #include "libirc_rfcnumeric.h"
+#include "libtwirc/src/libtwirc.h"
 
 #define NAME   "kaulmate"
 #define AUTHOR "domsson"
@@ -42,7 +43,7 @@ struct irc_context
 struct kaul_config
 {
 	char *host;
-	unsigned short port;
+	char *port;
 	char *chan;
 	char *nick;
 	char *pass;
@@ -85,7 +86,8 @@ int can_send()
 	return (get_time() - last_msg) >= MSG_INTERVAL;
 }
 
-int send_msg(irc_session_t *s, const char *msg)
+//int send_msg(irc_session_t *s, const char *msg)
+int send_msg(struct twirc_state *s, const char *msg)
 {
 	double now = get_time();
 	double delta = now - last_msg;
@@ -96,18 +98,13 @@ int send_msg(irc_session_t *s, const char *msg)
 		return 1;
 	}
 
-	struct kaul_config *cfg = irc_get_ctx(s);
-	if (irc_cmd_msg(s, cfg->chan, msg))
-	{
-		fprintf(stderr, "Could not send message. Is the connection still up?\n");
-		return 1;
-	}
-
+	twirc_cmd_privmsg(s, "#domsson", msg);
 	last_msg = now;
 	return 0;
 }
 
-void cmd_bot(irc_session_t *s)
+//void cmd_bot(irc_session_t *s)
+void cmd_bot(struct twirc_state *s)
 {
 	char msg[1024];
 	snprintf(msg, 1024, "I'm kaulmate, version %o.%o build %f by %s, see %s",
@@ -119,7 +116,8 @@ void cmd_bot(irc_session_t *s)
 	send_msg(s, msg);
 }
 
-void cmd_random(irc_session_t *s)
+//void cmd_random(irc_session_t *s)
+void cmd_random(struct twirc_state *s)
 {
 	FILE *fp = fopen("random", "r");
 	if (fp == NULL)
@@ -155,10 +153,12 @@ void cmd_random(irc_session_t *s)
 	send_msg(s, str);
 }
 
-void cmd_time(irc_session_t *s)
+void cmd_time(struct twirc_state *s)
 {
+	/*
 	// time_t is not guaranteed to be an int so it isn't
 	// exactly safe to add to it, but it seems safe enough.
+	
 	struct kaul_config *cfg = irc_get_ctx(s);
 	const time_t t = time(NULL) + (cfg->timezone * 60 * 60);
 	struct tm *gmt = gmtime(&t);
@@ -166,17 +166,17 @@ void cmd_time(irc_session_t *s)
 	char timestr[32];
 	snprintf(timestr, 32, "Current time: %02d:%02d (GMT+%d)", gmt->tm_hour, gmt->tm_min, cfg->timezone);
 	send_msg(s, timestr);
-	return;
+	*/
 }
 
-void cmd_youtube(irc_session_t *s)
+void cmd_youtube(struct twirc_state *s)
 {
 	// TODO obviously this will have to come out of a config file
 	const char yt[] = "https://www.youtube.com/channel/UCNYkFdQfHWKnTr6ko9XcJig";
 	send_msg(s, yt);
 }
 
-void handle_command(irc_session_t *s, const char *cmd)
+void handle_command(struct twirc_state *s, const char *cmd)
 {
 	if (strcmp(cmd, "!bot") == 0)
 	{
@@ -201,161 +201,56 @@ void handle_command(irc_session_t *s, const char *cmd)
 	cmd_random(s);
 }
 
-/*
- * This is our dummy event handler that we'll assign to events we're not interested in.
- */
-void event_null(irc_session_t *s, const char *e, const char *o, const char **p, unsigned c)
+void event_welcome(struct twirc_state *s, const struct twirc_event *evt)
 {
-	printf("event_null\n");
-	if (e)   printf("\tevent   = %u\n", e);
-	if (o)   printf("\torigin  = %s\n", o);
-	if (c>0) printf("\tparam 1 = %s\n", p[0]);
-	if (c>1) printf("\tparam 2 = %s\n", p[1]);
-	if (c>2) printf("\tparam 3 = %s\n", p[2]);
-}
-
-/*
- * Fired once we established a connection with the server.
- * This would be a good place to kick off other actions (joining channel etc)
- */
-void event_connect(irc_session_t *s, const char *e, const char *o, const char **p, unsigned c)
-{
-	printf("event_connect\n");
-	if (e)  printf("\tevent   = %s\n", e);
-	if (o)  printf("\torigin  = %s\n", o);
-
-	struct kaul_config *cfg = irc_get_ctx(s);
-
-	// TODO: figure out if we actually need this CAP at this time
-	if (irc_send_raw(s, "CAP REQ :twitch.tv/membership"))
-	{
-		fprintf(stderr, "Could not request membership capability.\n");
-	}
-
-	/*
-	 * TODO we don't request tags capability at this point, because
-	 * tags are a IRCv3 feature that isn't supported by libircclient.
-	 * We'll have to look for a different library, write our own or,
-	 * maybe even better, patch libircclient and send a push request.
-	 */
-	/*
-	if (irc_send_raw(s, "CAP REQ :twitch.tv/tags"))
-	{
-		fprintf(stderr, "Could not request tags capability.\n");
-	}
-	*/
-
-	// TODO: figure out if we actually need this CAP at this time
-	if (irc_send_raw(s, "CAP REQ :twitch.tv/commands"))
-	{
-		fprintf(stderr, "Could not request commands capability.\n");
-	}
-
-	if (irc_cmd_join(s, cfg->chan, 0))
-	{
-		fprintf(stderr, "Could not join channel %s\n", cfg->chan);
-	}
+	twirc_cmd_join(s, "#domsson");
 }
 
 /*
  * Someone joined the channel - it could be us!
  */
-void event_join(irc_session_t *s, const char *e, const char *o, const char **p, unsigned c)
+void event_join(struct twirc_state *s, const struct twirc_event *evt)
 {
-	printf("event_join\n");
-	if (e)   printf("\tevent   = %s\n", e);
-	if (o)   printf("\torigin  = %s\n", o);
-	if (c>0) printf("\tchannel = %s\n", p[0]);
+	if (strcmp(evt->nick, "kaulmate") == 0)
+	{
+		fprintf(stderr, "*** we joined %s\n", evt->channel);
+
+		if (strcmp(evt->channel, "#domsson") == 0)
+		{
+			twirc_cmd_privmsg(s, "#domsson", "jobruce is the best!");
+		}
+	}
 }
 
 /*
  * Someone left the channel - it could be us!
  */
-void event_part(irc_session_t *s, const char *e, const char *o, const char **p, unsigned c)
+void event_part(struct twirc_state *s, const struct twirc_event *evt)
 {
-	printf("event_part\n");
-	if (e)   printf("\tevent   = %s\n", e);
-	if (o)   printf("\torigin  = %s\n", o);
-	if (c>0) printf("\tchannel = %s\n", p[0]);
-}
-
-/*
- * Private messages - however, whispers don't seem to get through here
- */
-void event_privmsg(irc_session_t *s, const char *e, const char *o, const char **p, unsigned c)
-{
-	printf("event_privmsg\n");
-	if (e)   printf("\tevent   = %s\n", e);
-	if (o)   printf("\torigin  = %s\n", o);
-	if (c>0) printf("\treceip. = %s\n", p[0]);
-	if (c>1) printf("\tmessage = %s\n", p[1]);
-}
-
-/*
- * Handler for receiving notices - not sure this is relevant on Twitch.
- */
-void event_notice(irc_session_t *s, const char *e, const char *o, const char **p, unsigned c)
-{
-	printf("event_notice\n");
-	if (e)   printf("\tevent   = %s\n", e);
-	if (o)   printf("\torigin  = %s\n", o);
-	if (c>0) printf("\treceip. = %s\n", p[0]);
-	if (c>1) printf("\tmessage = %s\n", p[1]);
-}
-
-/*
- * This is where we intercept user's chat messages
- */
-void event_channel(irc_session_t *s, const char *e, const char *o, const char **p, unsigned c)
-{
-	printf("event_channel\n");
-	if (e)   printf("\tevent   = %s\n", e);
-	if (o)   printf("\torigin  = %s\n", o);
-	if (c>0) printf("\tchannel = %s\n", p[0]);
-	if (c>1) printf("\tmessage = %s\n", p[1]);
-
-	if (p[1][0] == '!') // This might be a command!
+	if (strcmp(evt->nick, "kaulmate") == 0)
 	{
-		handle_command(s, p[1]);
+		fprintf(stderr, "*** we left %s\n", evt->channel);
 	}
 }
 
-/*
- * This is the event that triggers for things like PING
- */
-void event_ctcp_rep(irc_session_t *s, const char *e, const char *o, const char **p, unsigned c)
+void event_privmsg(struct twirc_state *s, const struct twirc_event *evt)
 {
-	printf("event_ctcp_rep\n");
-	if (e)   printf("\tevent   = %s\n", e);
-	if (o)   printf("\torigin  = %s\n", o);
-	if (c>0) printf("\tchannel = %s\n", p[0]);
-	if (c>1) printf("\tmessage = %s\n", p[1]);
+	fprintf(stdout, "%s: %s\n", evt->nick, evt->message);
+
+	if (evt->message[0] == '!')
+	{
+		handle_command(s, evt->message);
+	}
 }
 
 /*
  * This is the event that triggers for /me messages
  */
-void event_ctcp_action(irc_session_t *s, const char *e, const char *o, const char **p, unsigned c)
+void event_action(struct twirc_state *s, const struct twirc_event *evt)
 {
-	printf("event_ctcp_action\n");
-	if (e)   printf("\tevent   = %s\n", e);
-	if (o)   printf("\torigin  = %s\n", o);
-	if (c>0) printf("\tchannel = %s\n", p[0]);
-	if (c>1) printf("\tmessage = %s\n", p[1]);
+	fprintf(stdout, "* %s %s\n", evt->nick, evt->message);
 }
 
-/*
- * These are rather specific events that we (mostly) do not care about
- */
-void event_numeric(irc_session_t *s, unsigned e, const char *o, const char **p, unsigned c)
-{
-	printf("event_numeric\n");
-	if (e)   printf("\tevent   = %u\n", e);
-	if (o)   printf("\torigin  = %s\n", o);
-	if (c>0) printf("\tparam 1 = %s\n", p[0]);
-	if (c>1) printf("\tparam 2 = %s\n", p[1]);
-	if (c>2) printf("\tparam 3 = %s\n", p[2]);
-}
 /*
  * Close session, disconnect, free objects, etc
  */
@@ -380,7 +275,7 @@ void cleanup(irc_session_t *s)
 	}
 }
 
-static int cfg_handler(void *config, const char *section, const char *name, const char *value)
+int cfg_handler(void *config, const char *section, const char *name, const char *value)
 {
 	struct kaul_config *cfg = (struct kaul_config*) config;
 
@@ -392,7 +287,7 @@ static int cfg_handler(void *config, const char *section, const char *name, cons
 	
 	if (str_equals(name, "port"))
 	{
-		cfg->port = atoi(value);
+		cfg->port = str_quoted(value) ? str_unquote(value) : strdup(value);
 		return 1;
 	}
 
@@ -471,7 +366,7 @@ int main(int argc, char *argv[])
 	dir_concat(general_cfg_path, PATH_MAX, CONFIG_GENERAL);
 	dir_concat(account_cfg_path, PATH_MAX, CONFIG_ACCOUNT);
 
-	struct kaul_config cfg = {};
+	struct kaul_config cfg = { 0 };
 
 	if (ini_parse(general_cfg_path, cfg_handler, &cfg) < 0)
 	{
@@ -500,12 +395,12 @@ int main(int argc, char *argv[])
 		return EXIT_FAILURE;
 	}
 
-	if (cfg.port == 0)
+	if (cfg.port == NULL)
 	{
 		fprintf(stderr, 
 			"IRC port ('port') not specified in %s, using default.\n", 
 			CONFIG_GENERAL);
-		cfg.port = (strchr(cfg.host, ':') == 0) ? 6667 : 0;
+		cfg.port = (strchr(cfg.host, ':') == 0) ? "6667" : NULL;
 	}
 
 	if (cfg.nick == NULL)
@@ -524,37 +419,7 @@ int main(int argc, char *argv[])
 		return EXIT_FAILURE;
 	}
 
-	// The IRC callbacks structure
-	irc_callbacks_t callbacks;
-
-	// Init it (is this really needed?)
-	memset(&callbacks, 0, sizeof(callbacks));
-
-	// Set up the event callbacks
-	callbacks.event_connect =  event_connect;
-	callbacks.event_join =     event_join;
-	callbacks.event_nick =     event_null;
-	callbacks.event_quit =     event_null;
-	callbacks.event_part =     event_part;
-	callbacks.event_mode =     event_null;
-	callbacks.event_topic =    event_null;
-	callbacks.event_kick =     event_null;
-	callbacks.event_channel =  event_channel;
-	callbacks.event_privmsg =  event_privmsg;
-	callbacks.event_notice =   event_notice;
-	callbacks.event_invite =   event_null;
-	callbacks.event_umode =    event_null;
-	callbacks.event_unknown =  event_null;
-	callbacks.event_numeric =  event_numeric;
-
-	callbacks.event_ctcp_rep =     event_ctcp_rep;
-	callbacks.event_ctcp_action =  event_ctcp_action;
-	//callbacks.event_dcc_chat_req = event_null_dcc;
-	//callbacks.event_dcc_send_req = event_null_dcc;
-
-	// Now create the session
-	irc_session_t  *session;
-	session = irc_create_session(&callbacks);
+	struct twirc_state *session = twirc_init();
 
 	if (!session)
 	{
@@ -562,29 +427,21 @@ int main(int argc, char *argv[])
 		return EXIT_FAILURE;
 	}
 
-	// Have libirc turn complex nick strings into nicks
-	irc_option_set(session, LIBIRC_OPTION_STRIPNICKS);
-
-	// Save the context info into the session for easy retrieval later
-	irc_set_ctx(session, &cfg);
-
-	// Initiate the IRC server connection
-	if (irc_connect(session, cfg.host, cfg.port, cfg.pass, cfg.nick, cfg.nick, cfg.nick))
-	{
-		fprintf(stderr, "Could not connect: %s\n", irc_strerror(irc_errno(session)));
-		cleanup(session);
-		return EXIT_FAILURE;
-	}
+	struct twirc_callbacks *cbs = twirc_get_callbacks(session);
 	
-	// Run endless loop that will fire events as they occur
-	if (irc_run(session))
+	cbs->welcome = event_welcome;
+	cbs->privmsg = event_privmsg;
+
+	if (twirc_connect(session, cfg.host, cfg.port, cfg.pass, cfg.nick) != 0)
 	{
-		fprintf(stderr, "Could not connect or I/O error: %s\n", irc_strerror(irc_errno(session)));
-		cleanup(session);
+		fprintf(stderr, "Could not connect to IRC\n");
 		return EXIT_FAILURE;
 	}
 
-	cleanup(session);
+	twirc_loop(session, 1000);
+	twirc_kill(session);
+	free_cfg(&cfg);
+
 	fprintf(stderr, "Good bye!\n");
 	return EXIT_SUCCESS;
 }
